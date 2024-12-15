2b87800430fd5bda6d16b94a7a8dccb84f77d2f9
Evgeny Ryabchenko <dev2@max.id>
2024-12-15 22:31:05 +0300

    исправлены ошибки, включены доп.тесты

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -16,27 +16,33 @@ func Unpack(input string) (string, error) {
 
 	for _, symbol := range input {
 		switch {
+		case escaped:
+			// Если символ экранирован, он должен быть либо цифрой, либо '\'
+			if symbol != '\\' && !unicode.IsDigit(symbol) {
+				return "", ErrInvalidString
+			}
+			prevRune = symbol
+			escaped = false
+
+		case symbol == '\\':
+			escaped = true
+			result.WriteRune(prevRune)
+
 		case unicode.IsDigit(symbol):
-			if prevRune == 0 || escaped {
-				return "", errors.New("invalid string")
+			if prevRune == 0 {
+				return "", ErrInvalidString
 			}
 			count, _ := strconv.Atoi(string(symbol))
 			result.WriteString(strings.Repeat(string(prevRune), count))
 			prevRune = 0
-			escaped = false
-
-		case symbol == '\\' && !escaped:
-			escaped = true
 
 		default:
 			if prevRune != 0 {
 				result.WriteRune(prevRune)
 			}
 			prevRune = symbol
-			escaped = false
 		}
 	}
-
 	if prevRune != 0 {
 		result.WriteRune(prevRune)
 	}
