39f101bf41618aab9d31d8c7d9e44f7fccd1088b
Evgeny Ryabchenko <dev2@max.id>
2024-12-15 21:44:44 +0300

    2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,44 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var result strings.Builder
+	var prevRune rune
+	escaped := false
+
+	for _, symbol := range input {
+		switch {
+		case unicode.IsDigit(symbol):
+			if prevRune == 0 || escaped {
+				return "", errors.New("invalid string")
+			}
+			count, _ := strconv.Atoi(string(symbol))
+			result.WriteString(strings.Repeat(string(prevRune), count))
+			prevRune = 0
+			escaped = false
+
+		case symbol == '\\' && !escaped:
+			escaped = true
+
+		default:
+			if prevRune != 0 {
+				result.WriteRune(prevRune)
+			}
+			prevRune = symbol
+			escaped = false
+		}
+	}
+
+	if prevRune != 0 {
+		result.WriteRune(prevRune)
+	}
+
+	return result.String(), nil
 }
