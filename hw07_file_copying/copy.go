695a491105a25563ed693ea097b996666a0e4c6f
Evgeny Ryabchenko <dev2@max.id>
2025-03-03 15:37:02 +0300

    1

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +16,100 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	// Проверяем, что указаны пути к файлам
+	if fromPath == "" || toPath == "" {
+		return fmt.Errorf("не указаны пути к исходному и целевому файлам")
+	}
+
+	// Получаем информацию о исходном файле
+	srcFile, err := os.Open(fromPath)
+	if err != nil {
+		return fmt.Errorf("Ошибка при открытии исходного файла: %v\n", err)
+	}
+	defer func() {
+		err := srcFile.Close()
+		if err != nil {
+			fmt.Printf("Ошибка при закрытии исходного файла: %v\n", err)
+		}
+	}()
+
+	fileInfo, err := srcFile.Stat()
+	if err != nil {
+		return fmt.Errorf("Ошибка при получении информации о файле: %v\n", err)
+	}
+
+	// Проверяем, что offset не превышает размер файла
+	if offset > fileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	// Проверяем, является ли файл специальным
+	if fileInfo.Mode()&os.ModeNamedPipe != 0 || fileInfo.Mode()&os.ModeDevice != 0 {
+		return ErrUnsupportedFile
+	}
+
+	fileSize := fileInfo.Size()
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	// Устанавливаем смещение
+	if _, err := srcFile.Seek(offset, io.SeekStart); err != nil {
+		return fmt.Errorf("Ошибка при установке смещения: %v\n", err)
+	}
+
+	// Определяем количество байт для копирования
+	bytesToCopy := fileInfo.Size() - offset
+	if limit > 0 && limit < bytesToCopy {
+		bytesToCopy = limit
+	}
+
+	// Открываем целевой файл для записи
+	dstFile, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("Ошибка при создании целевого файла: %v\n", err)
+	}
+	defer func() {
+		err := dstFile.Close()
+		if err != nil {
+			fmt.Printf("Ошибка при закрытии целевого файла: %v\n", err)
+		}
+	}()
+
+	// Копируем данные с отображением прогресса
+	buffer := make([]byte, 4096)
+	var totalCopied int64
+	progress := pb.Full.Start64(limit)
+	progress.Start()
+	for {
+		readedBytes, err := srcFile.Read(buffer)
+		if err != nil && err != io.EOF {
+			return fmt.Errorf("Ошибка при чтении из исходного файла: %v\n", err)
+		}
+		if readedBytes == 0 {
+			break // Достигнут конец файла
+		}
+
+		// Проверяем, не превышает ли количество скопированных байт лимит
+		if totalCopied+int64(readedBytes) > bytesToCopy {
+			readedBytes = int(bytesToCopy - totalCopied) // Ограничиваем количество копируемых байт
+		}
+
+		// Записываем данные в целевой файл
+		if _, err := dstFile.Write(buffer[:readedBytes]); err != nil {
+			return fmt.Errorf("Ошибка при записи в целевой файл: %v\n", err)
+		}
+
+		totalCopied += int64(readedBytes)
+
+		// Выводим прогресс
+		progress.SetCurrent(totalCopied)
+		time.Sleep(time.Millisecond * 1000)
+		if totalCopied >= bytesToCopy {
+			break
+		}
+	}
+	progress.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,69 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"path/filepath"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	tmpDir := "/tmp"
+	if err := os.MkdirAll(tmpDir, 0755); err != nil {
+		t.Fatalf("Не удалось создать временную директорию: %v", err)
+	}
+
+	// Путь к директории с исходными файлами
+	srcDir := "./testdata"
+
+	// Получаем список всех файлов в директории testdata
+	files, err := os.ReadDir(srcDir)
+	if err != nil {
+		t.Fatalf("Ошибка при чтении директории testdata: %v", err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue // Пропускаем поддиректории
+		}
+
+		srcFilePath := filepath.Join(srcDir, file.Name())
+		dstFilePath := filepath.Join(tmpDir, file.Name())
+
+		t.Run("test coping file: "+file.Name(), func(t *testing.T) {
+			err := Copy(srcFilePath, dstFilePath, 0, 0)
+			if err != nil {
+				t.Fatalf("Ошибка при копировании файла %s: %v", srcFilePath, err)
+			}
+
+			srcFileInfo, err := os.Stat(srcFilePath)
+			if err != nil {
+				t.Fatalf("Ошибка при получении информации о исходном файле %s: %v", srcFilePath, err)
+			}
+
+			dstFileInfo, err := os.Stat(dstFilePath)
+			if err != nil {
+				t.Fatalf("Ошибка при получении информации о целевом файле %s: %v", dstFilePath, err)
+			}
+
+			if dstFileInfo.Size() != srcFileInfo.Size() {
+				t.Errorf("Размеры файлов не совпадают для %s: ожидается %d, получено %d", file.Name(), srcFileInfo.Size(), dstFileInfo.Size())
+			}
+			assert.NoError(t, err)
+
+		})
+
+	}
+
+	// Удаляем временные файлы
+	for _, file := range files {
+		if file.IsDir() {
+			continue // Пропускаем поддиректории
+		}
+		dstFilePath := filepath.Join(tmpDir, file.Name())
+		err := os.Remove(dstFilePath)
+		if err != nil {
+			t.Fatalf("Ошибка при удалении временного файла %s: %v", dstFilePath, err)
+		}
+	}
 }
