15add7cc5cddcce8b51b458441cf7a18f820107d
Evgeny Ryabchenko <dev2@max.id>
2025-04-17 11:31:10 +0300

    - //nolint:critic

--- hw12_13_14_15_16_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_16_calendar/cmd/calendar/main.go
@@ -93,7 +93,7 @@ func main() {
 			if err := server.Start(ctx); err != nil {
 				logg.Error("failed to start http server: " + err.Error())
 				cancel()
-				os.Exit(1) //nolint:critic
+				os.Exit(1)
 			}
 		},
 	}
