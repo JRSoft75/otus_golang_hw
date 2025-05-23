d69dd7b5a56edf3a1b4a10c41002e481115b5ed1
Evgeny Ryabchenko <dev2@max.id>
2025-05-23 11:55:19 +0300

    v1

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,80 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", 10*time.Second, "connection timeout")
+	flag.Parse()
+
+	if flag.NArg() != 2 {
+		fmt.Println("Usage: go-telnet [--timeout=duration] host port")
+		os.Exit(1)
+	}
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT)
+	defer stop()
+
+	address := net.JoinHostPort(host, port)
+	client := NewTelnetClient(address, *timeout, os.Stdin, os.Stdout)
+
+	err := client.Connect()
+	if err != nil {
+		fmt.Printf("Connection error: %v\n", err)
+		return
+	}
+	defer func(client TelnetClient) {
+		_ = client.Close()
+	}(client)
+
+	fmt.Printf("Connected to %s\n", address)
+
+	done := make(chan struct{})
+
+	// Горутина для чтения данных из сокета и вывода в STDOUT
+	go func() {
+		err := client.Receive()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				fmt.Println("\nServer closed the connection.")
+			} else {
+				fmt.Printf("\nReceive error: %v\n", err)
+			}
+		}
+		close(done)
+	}()
+
+	// Горутина для записи данных из STDIN в сокет
+	go func() {
+		err := client.Send()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				fmt.Println("\nEOF detected. Closing connection.")
+			} else {
+				fmt.Printf("\nSend error: %v\n", err)
+			}
+		}
+		close(done)
+	}()
+
+	// Ожидание завершения работы программы
+	select {
+	case <-done:
+		fmt.Println("Connection closed.")
+	case <-ctx.Done():
+		fmt.Println("\nInterrupt signal received. Exiting...")
+	}
 }
